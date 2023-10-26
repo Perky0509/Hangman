239956c0d8801079fc04e318b146fb9b24c6bcb0
Perky0509 <s.boswallperks@gmail.com>
2023-10-26 14:11:10 +0100

    edited milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,34 +1,26 @@
 import random
 
+
 word_list = ['melon', 'grapes', 'strawberries', 'figs', 'kiwi']
- 
-
-
                 
 def check_user_guess(user_letter_guess):
     random_word = random.choice(word_list)
-    #guess = input(f'Please enter a single letter:').lower()
     if user_letter_guess in random_word:
         print(f"Good guess! {user_letter_guess} is in the word.")
-
     else: 
         print(f"Sorry, {user_letter_guess} is not in the word. Try again.")
-    
     return random_word
 
 
 def ask_for_input():
-
     while True:
         user_letter_guess = input(f'Please enter a single letter:')
         if len(user_letter_guess) != 1 and user_letter_guess.isalpha():
             print("Invalid letter. Please, enter a single alphabetical character.")
         else:
             break 
-    
-    return check_user_guess(user_letter_guess)
+        return check_user_guess(user_letter_guess)
       
-
 word_list = ['melon', 'grapes', 'strawberries', 'figs', 'kiwi']
 ask_for_input()
-#check_guess(guess)
+
