30f2e67451640a007c043bc0f1b68f83b9b83ade
Perky0509 <s.boswallperks@gmail.com>
2023-10-26 14:04:59 +0100

    edited milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,27 +1,27 @@
 import random
 
+
 word_list = ('melon', 'grapes', 'strawberries', 'figs', 'kiwi')
+
 class GuessTheWord:
         
-        def __init__(self, word_list = []):
-            self.word_list = word_list
-            #self.guess = guess 
-        
-        def word_options(self):
-            return self.word_list
+    def __init__(self, word_list = []):
+        self.word_list = word_list 
+    
+    def word_options(self):
+        return self.word_list
 
-        def random_word(self): 
-            word = random.choice(self.word_list)
-            return word
+    def random_word(self): 
+        word = random.choice(self.word_list)
+        return word
 
-        def user_guess(self): 
-            guess = input()
-            print(f'Please enter a single letter:', guess)
-            if len(guess) == 1 and guess.isalpha():
-                return "Good guess!"
-
-            else:
-                return "oops! That is not a valid input."
+    def user_guess(self): 
+        guess = input()
+        print(f'Please enter a single letter:', guess)
+        if len(guess) == 1 and guess.isalpha():
+            return "Good guess!"
+        else:
+            return "oops! That is not a valid input."
            
 
 first_list = GuessTheWord(['melon', 'grapes', 'strawberries', 'figs', 'kiwi'])
